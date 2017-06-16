1c89b27c71bd54d615d922e226269f4a86e77fab
David Lowry-Duda <david@loweryduda.com>
2017-06-16 10:48:49 +0100

    Change pyflakes to use sage's pyflakes (py2)
    This is for my own repo only.

--- test.sh
+++ test.sh
@@ -35,7 +35,7 @@ else
 fi
 
 echo "Running pyflakes..."
-read PYFLAKES_ERRCNT < <(find . | grep "\.py$" | xargs pyflakes 2>&1 | tee /dev/stderr | grep "py:" -c)
+read PYFLAKES_ERRCNT < <(find . | grep "\.py$" | xargs sage -python -m pyflakes 2>&1 | tee /dev/stderr | grep "py:" -c)
 if [[ $PYFLAKES_ERRCNT > 0 ]]; then
   echo "WARNING: pyflakes reported $PYFLAKES_ERRCNT error(s)"
 else
